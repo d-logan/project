eb528a537c312b9a3fe6762e3513aa7cd68b17db
d-logan <davidblogan163@gmail.com>
2021-07-01 20:10:47 -0400

    fuck

--- tests/java.js
+++ tests/java.js
@@ -13,3 +13,21 @@ const fadeEffect = setInterval(() => {
 }, 200);
 
 window.addEventListener('load', fadeEffect);
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
